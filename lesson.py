9122dec7277da8017838dc55f9067c4ddc4e1446
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-07-21 08:02:44 +0900

    0721

--- lesson.py
+++ lesson.py
@@ -1,9 +1,20 @@
 # coding: utf-8
 # if文による条件分岐　elif文
-number = 1
+
+# ランダムをインポートして機能をつかえるようにしている
+import random 
+# ランダムを利用する（今回は1〜3までの数値を表す）
+print ("じゃんけんマシーンだよ")
+print ("1：グー 2：チョキ 3：パー")
+print ("じゃんけーん")
+print ("1〜3までの数字を入れてください")
+get = int(input())
+number = random.randint(1,3)
+
 if number == 1:
-	print( "スキ！")	#条件式が成立したときの処理
+	print( "あなたの勝ち")	#条件式が成立したときの処理
 elif number == 2:
-  print("どちらでもない")
+  print("あなたの負け")
 else:
-	print( "キライ")	#条件式が成立しなかったときの処理
+	print( "あいこ")	#条件式が成立しなかったときの処理
+
