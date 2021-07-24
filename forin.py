d2fa6a2385b29dda5f0053e1c8b692fe458c2f6f
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-07-24 09:16:48 +0900

    for i 表現

---
+++ forin.py
@@ -0,0 +1,8 @@
+#rangeを利用することで5までを表現している
+#for XXX in 範囲 
+for i in range(5):
+  print("hello:" + str(i+1))
+
+# この場合10未満という意味合い
+for i in range(5,10):
+  print("hello:" + str(i+1))
--- heisei_change.py
+++ heisei_change.py
@@ -1,5 +1,11 @@
 # 西暦年から平成年を求める
-print("西暦を平成に直します。1989以上の数値を入力してください")
+import datetime
+kotosi = datetime.date.today().year
+print("さて今年は" + str(kotosi) + "年ですが、西暦を平成に直します。1989以上の数値を入力してください")
 seireki = int(input())
-heisei = seireki - 1988
-print("平成"+ str(heisei) + "年です")
+if seireki >= 1989:
+  print("あなたの調べたい西暦" + str(seireki) + "年は、", end="")
+  heisei = seireki - 1988
+  print("平成"+ str(heisei) + "年です")
+else:
+  print("そんな西暦ありません")
