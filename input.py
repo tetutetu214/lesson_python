ba5b39046fddce9e7406602207528b92fe86447d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-07-24 10:10:39 +0900

    複数回受け取りする

--- input.py
+++ input.py
@@ -2,9 +2,17 @@
 #もともとUNIXの標準入力を指している
 
 #文字として受け取り
-int = input()
+# int = input()
 
 #数値を数値として受け取る
-num = int(input())
-print(num)
+# num = int(input())
+# print(num)
+
+#複数業を受け取る(受け取り個数が分かっている場合)
+#.rstripは、改行の削除
+
+count = int(input())
+for i in range(count):
+  line = input().rstrip()
+  print(line)
 
