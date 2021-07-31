8b751c72949ea5b6e110e403530a779d2d2fe703
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-07-31 11:17:10 +0900

    fizzbizz

---
+++ 0731.py
@@ -0,0 +1,10 @@
+num = int(input())
+for i in range(1,num+1):
+ if i % 3 == 0 and i % 5 == 0:
+   print("Fizz Buzz")
+ elif i % 3 == 0:
+   print("Fizz")
+ elif i % 5 == 0:
+   print("Buzz")
+ else:
+   print(i)
