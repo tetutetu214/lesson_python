64c7aae7e65efbed966051e51171faa6c2c10e3e
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-03 08:22:39 +0900

    for文

---
+++ 0803_for.py
@@ -0,0 +1,30 @@
+#繰り返し
+
+str = "Hello Paiza"
+for i in range(100):
+	print(str)
+
+#回数はrangeで設定する
+num = int(input())
+string = "Hello paiza"
+for i in range(num):
+ print(string)
+
+#繰り返しのfor文章の中のインデントifは揃える
+print(count)
+for i in range(count):
+  num = int(input())
+  if num == 0:
+        print(str(num) + "は0")
+  elif num > 0:
+        print(str(num) + "はプラス")
+  else:
+        print(str(num) + "はマイナス")
+
+i = input()
+print(i)
+
+#1000文字受け取り出力する
+for i in range(1000):
+  i = input()
+  print(i)
