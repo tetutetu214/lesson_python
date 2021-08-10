031e4ca75a5981aa9b8d78e87549416383eccaee
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-10 22:01:59 +0900

    コメントアウト

--- 0810.py
+++ 0810.py
@@ -6,10 +6,12 @@ def compare(val1, val2):
   else:
     return -1
 
+#引数aの倍数のうち、引数b以下のものの合計値を算出するメソッド
 def multiple_total(a, b):
   i = 0
   total = 0
   while i <= b:
+#aの倍数とは即ち、自身で割って余りが0になるものを指す
     if i % a == 0:
       total += i
     i += 1
