53cf80bb4a4d7657efe16eb4225327cf9b2dc4e6
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-31 07:55:08 +0900

    割り算

---
+++ 0831.py
@@ -0,0 +1,19 @@
+num = [int(x) for x in input().split()]
+print(sum(num))
+
+num = [int(x) for x in input().split()]
+print(max(num))
+
+num = [int(x) for x in input().split() ]
+one = num[0] % 10
+two = num[1] % 10
+if one > two:
+  print(num[1])
+else:
+  print(num[0])
+
+num = [int(x) for x in input().split()]
+if num[0] % num[1] == 0:
+  print("Yes")
+else:
+  print("No")
