4e63e18adaaa46f136ae709c9fa396403bab1552
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-06 22:03:31 +0900

    0905

--- 0905.py
+++ 0905.py
@@ -1,3 +1,4 @@
+#右と左の文字をn回受け取り、多い方を出力する
 n = int(input())
 st = [input() for i in range(n)]
 le = st.count('left')
@@ -9,12 +10,15 @@ elif le < ri:
 else:
   print('same')
 
-
-
-
+#数字を受ける、リストの中にbの値があれば”Yes”
 a,b = map(int, input().split())
 num = [int(x) for x in input().split()]
 if b in num:
     print("Yes")
 else:
     print("No")
+
+#値を受けとり、リストの中にbの値がいくつあるかカウントする
+a, b = map(int,input().split())
+num = [int(x) for x in input().split()]
+print(num.count(b))
