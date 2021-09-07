a09c1d2dfe5b0da27aaead094e34e9a84663c754
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-08 08:43:32 +0900

    0907

--- 0907.py
+++ 0907.py
@@ -6,3 +6,11 @@ for i in range(a):
     if num[i] == b:
       pos = i
 print(pos)
+
+a = int(input())
+num = [int(x) for x in input().split]
+count = 0
+for i in range(1,a):
+	if num[i-1] < num[i]:
+		count +=1
+print(count)
