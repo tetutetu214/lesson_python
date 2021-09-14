d1401d0ace616506dee447f6deef70a3d07a7438
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-15 08:11:07 +0900

    0915

--- 0915.py
+++ 0915.py
@@ -11,3 +11,14 @@ for i in range(9):
 		index=i
 ans = index + 1
 print(ans)
+
+
+#数字全探索
+N = int(input())
+
+count = 0
+
+for i in range(1,N+1):
+  if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
+      count += 1
+print(count)
