c9fe9beb21e1918ae069d0ff42e5a976c019ae7f
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-15 08:11:20 +0900

    0915

--- 0915.py
+++ 0915.py
@@ -22,3 +22,11 @@ for i in range(1,N+1):
   if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
       count += 1
 print(count)
+
+N=int(input())
+
+count = 0
+for i in range(1,N+1):
+  if N % i == 0:
+    count += 1
+print(count)
