552ba04423707f65d0a6d6ecc3bb89f7cb60a54f
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-16 07:44:54 +0900

    FizzBuzz

--- 0916.py
+++ 0916.py
@@ -6,3 +6,14 @@ for i in range(1,max(A,B)+1) :
         ans = i
 print(ans)
 
+N = int(input())
+
+for i in range(1,N+1):
+  if i % 3 == 0 and i % 5 == 0:
+      print("FizzBuzz")
+  elif i % 3 == 0:
+      print("Fizz")
+  elif i % 5 == 0:
+      print("Buzz")
+  else:
+      print(i)
