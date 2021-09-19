269454c2756913f556f7a7d29d7f773430f5e561
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-19 11:43:11 +0900

    shell

--- 0918_2.sh
+++ 0918_2.sh
@@ -4,15 +4,14 @@ echo "=== userprofile ==="
 
 ROOT_DIR=/var/www/PhotoApp/iwphoto
 RAILS_SERVE_STATIC_FILES=1
-CMD='unicorn_rails -c config/unicorn.rb -E production -D'
+UNI_DIR=/home/ec2-user/.rbenv/shims/unicorn_rails
+CMD=${UNI_DIR} -c config/unicorn.rb -E production -D
 
 echo "=== unicorncommand ==="
 cd $ROOT_DIR && $CMD
 
-echo "=== unicorncommand ==="
-cd &ROOT_DIR && mkdir sample
+echo "=== mkdir ==="
+cd $ROOT_DIR && mkdir sample
 
-
-
-# echo "=== log ==="
-# echo $(curl http://169.254.169.254/latest/meta-data/instance-id) >> /tmp/test
+echo "=== log ==="
+echo $(curl http://169.254.169.254/latest/meta-data/instance-id) >> /tmp/test
