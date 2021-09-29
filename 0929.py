36b40a6c4d82b9abaa0cf96e40f0941825ac8e4b
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-09-30 08:21:54 +0900

    0930.py

--- 0929.py
+++ 0929.py
@@ -33,3 +33,16 @@ for x in range(1, N+1):
 
 # 答えを出力する
 print(count)
+
+N,K =map(int,input().split())
+count=0
+for x in range(1,N+1):
+  add=0
+  for i in range(1,x+1):
+      if x % i == 0:
+          add += 1
+      
+      if add == K:
+          count += 1
+
+print(count) 
