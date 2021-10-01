242809c1690f61d2073722e7e62794c7d30d4b0d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-10-02 08:54:34 +0900

    1002

--- 1002.py
+++ 1002.py
@@ -9,3 +9,23 @@ for i in S:
 			if add < 2:
 				count += 1
 print(count)
+
+# データを受け取る
+S = input()
+N = len(S)
+
+# 文字の全探索
+count = 0
+for x in range(ord('a'), ord('z')+1):
+    c = chr(x)
+    # S に c が含まれるかを調べる
+    flag = False
+    for i in range(N):
+        if S[i] == c:
+            flag = True
+    # S に c が含まれるならば 1 を足す
+    if flag:
+        count += 1
+
+# 答えを出力する
+print(count)
