b3f6cf2fe43bd49425a643b3bd37b2f8dbb069f3
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-10-01 23:02:37 +0900

    1001

--- 0930.py
+++ 0930.py
@@ -3,13 +3,14 @@ L,R=map(int,input(),split())
 count=0
 
 for x in range(L,R+1):
+
   S = str(x)
   flag = True
   N = len(S)
   for i in range(N):
     if S[i] != S[(N-1)-i]:
-			flag = False
-	if flag:
-		count += 1
-		
+      flag = False
+  if flag:
+    count += 1
+
 print(count)
